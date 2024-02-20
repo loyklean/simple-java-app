d85bff7588af6bf589b51b94de60f2f72860987f
Loyklean <loyklean@gmail.com>
2024-02-20 16:38:34 +0800

    Changing in the local project folder~

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenskins is sucks! I will never use it again!");
+        System.out.println("Jenskins is cool! I will use it again!");
     }
 
     public String getMessage() {
