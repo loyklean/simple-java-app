4420d58328cdaa5b4db21ae656f57e09954c8eb9
Loyklean <loyklean@gmail.com>
2024-02-21 14:16:08 +0800

    Commit with latest changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Everything is ok again !");
+        System.out.println("Everything is ok again ... This is the latest change !");
     }
 
     public String getMessage() {
